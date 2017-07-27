0b3db590cafa7b29e736a35047078149008fac6d
Anna J <ajeszeck@gmail.com>
2017-07-27 12:57:50 -0700

    Add nested data structure of school classes

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,28 @@
+school = {
+	'gym': [
+		'teacher': 'Mr.Feeny',
+		'number_of_students': 20,
+		'student_of_the_week': 'Elijah Stringer'
+	],
+	'social_studies': [
+		'teacher': 'Johnny Castle'
+		'number_of_students': 35,
+		'student_of_the_week': 'Wynona Westcott'
+	],
+	'history': [
+		'teacher': 'Dewey Finn'
+		'number_of_students': 15,
+		'student_of_the_week': 'Cullen Ackerman'
+	],
+	'math': [
+		'teacher': 'Ms. Norbury'
+		'number_of_students': 25,
+		'student_of_the_week': 'Hartley Pocock'
+	],
+	'biology': [
+		'teacher': 'Charles Xavier'
+		'number_of_students': 30,
+		'student_of_the_week': 'Spring Danniel'
+	]
+}
+
