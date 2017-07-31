f50e4985d3e24e9c8057817c09260e356507dffa
Anna J <ajeszeck@gmail.com>
2017-07-30 22:23:03 -0700

    Add comments regarding outline of puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# SPECIES ------------------------
+# Puppy
+
+# CHARACTERISTICS ----------------
+# Friendly: yes
+# Number of Legs: 4
+# Name: varies
+# Color: varies
+# Hair Length: varies
+# Barks: yes
+
+# BEHAVIOR -----------------------
+# Nip
+# Cuddle
+# Nuzzle
+# Chew
+# Run
+
