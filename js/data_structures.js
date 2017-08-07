efc96b9003e36f4422fa708a4e52041007f8fe87
Anna J <ajeszeck@gmail.com>
2017-08-07 11:38:31 -0700

    Add colors array and names array and push items to both

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ['blue', 'green', 'red', 'yellow']
+var names = ['wendy', 'flicker', 'bob', 'frank']
+
+colors.push('purple');
+names.push('sally');
+
+console.log(colors);
+console.log(names);
+
