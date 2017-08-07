ec2542d7662716e3eed5f85adb32c4de5316883c
Anna J <ajeszeck@gmail.com>
2017-08-06 19:06:50 -0700

    Remove one test that was written wrong

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,15 +1,15 @@
 require_relative 'game.rb'
 
 describe Game do
-  let(:game) {Game.new("test")}
+  let(:game) {Game.new("Test")}
 
   it "checks if a letter is present in test" do
     expect(game.check_guess('e')).to eq "_e__"
   end
 
-  # it "checks lower/uppercase letter" do
-  #   expect(game.check_guess('T')).to eq "t___"
-  # end
+  it "checks lower/uppercase letter" do
+    expect(game.check_guess('T')).to eq "t__t"
+  end
 
   it "check if a letter present twice works" do
     expect(game.check_guess('t')).to eq "t__t"
