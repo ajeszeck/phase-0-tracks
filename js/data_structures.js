0e135843df5f42b8f67fc9c1ef64807ffbda58c3
Anna J <ajeszeck@gmail.com>
2017-08-07 12:25:53 -0700

    Add constructor function to create a prototype for an object. Call multiple copies of object

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,3 +14,23 @@ for (i = 0; i < colors.length; i++) {
 }
 
 console.log(horses);
+
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.speed = function() {
+    console.log("We're going fast!");
+  };
+}
+
+var newCar = new Car('kia', 'rio', 2010);
+console.log(newCar);
+newCar.speed();
+console.log('-----');
+
+var anotherNewCar = new Car('ford', 'taurus', 1996);
+console.log(anotherNewCar.make);
+anotherNewCar.speed();
+
+
