db9f51478ad955500495aab59d3a5480819ce253
Anna J <ajeszeck@gmail.com>
2017-08-07 11:47:39 -0700

    Add loops to create an object using two arrays to create key/value pairs

--- js/data_structures.js
+++ js/data_structures.js
@@ -7,3 +7,10 @@ names.push('sally');
 console.log(colors);
 console.log(names);
 
+var horses = {}
+
+for (i = 0; i < colors.length; i++) {
+  horses[names[i]] = colors[i];
+}
+
+console.log(horses);
