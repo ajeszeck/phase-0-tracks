4edc6a645becf4b309811f6b77d3e7eac1dc0ecd
Anna J <ajeszeck@gmail.com>
2017-08-08 16:30:48 -0700

    Add pseudocode to create a function that reverses strings

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// REVERSE FUNCTION
+// INPUT: Store a word given to to the function as a variable
+// STEPS:
+  //Use a loop to cycle through the letters of the word from back to front
+  // Storing the current letter to a new array as the loop runs
+// OUTPUT: Return the new reversed string
+
