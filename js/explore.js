44e5746670b89937dac295a8138403b780b0bc21
Anna J <ajeszeck@gmail.com>
2017-08-08 16:57:06 -0700

    Add function to reverse a string, add driver code with if/else block to activate function call

--- js/explore.js
+++ js/explore.js
@@ -5,3 +5,19 @@
   // Storing the current letter to a new array as the loop runs
 // OUTPUT: Return the new reversed string
 
+function reverse(string) {
+  var reversed_word = "";
+  for (var i = string.length - 1; i > -1; i--) {
+    reversed_word += string[i];
+    console.log(string[i]);
+  }
+
+  return reversed_word;
+}
+
+if (3 > 2) {
+  var word = reverse("hello");
+  console.log(word)
+} else {
+  console.log("I will not complete your task.")
+}
