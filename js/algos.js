8c1913e0d156a3dcabaf1264e17f291bf5af4bcd
Anna J <ajeszeck@gmail.com>
2017-08-08 17:26:17 -0700

    Add pseudocode for method that selects the longest word in an array of strings

---
+++ js/algos.js
@@ -0,0 +1,16 @@
+// ------ PSUEDOCODE FOR LONGEST WORD METHOD -----
+// INPUT: array
+// STEPS:
+//  - create a variable with an empty string to hold the longestWord
+//  - loop through each item of the array
+//  - each time count the length of the current word
+//  - use if/else block to test if the length of the current word
+//  is longer than the word currently stored in the longestWord variable
+//  - at the end of the loop (the end of the array), the value in the longestWord
+//  variable should be the longest of the whole array
+// OUTPUT: return string of longest word in array
+
+
+function longestWord(array) {
+
+}
