4a66821217ac6e32460842b607ab1cd7f41273b1
Anna J <ajeszeck@gmail.com>
2017-08-19 08:46:23 -0700

    Add some driver code to handle what action a user wants to take on address_book database

--- databases/address_book/address_book.rb
+++ databases/address_book/address_book.rb
@@ -8,7 +8,7 @@
 require 'sqlite3'
 require 'Faker'
 
-book = SQLite3::Database.new("address_book.db")
+db = SQLite3::Database.new("address_book.db")
 
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS address_book(
@@ -18,4 +18,68 @@ create_table_cmd = <<-SQL
     phone_number INT
   )
 SQL
+
+db.execute(create_table_cmd)
+
+def create_address(address_book, name, address, phone_number)
+  address_book.execute("INSERT INTO address_book (name, address, phone_number) VALUES (?, ?, ?)", [name, address, phone_number])
+  puts name
+  puts address
+  puts phone_number
+end
+
+def add_contact(address_book, name, address, phone_number)
+  address_book.execute("INSERT INTO address_book (name, address, phone_number) VALUES (?, ?, ?)", [name, address, phone_number])
+end
+
+address = "#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.state}, #{Faker::Address.zip_code}"
+# create_address(db, Faker::Name.name, address, Faker::Number.number(10))
+
+# 15.times do
+#   address = "#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.zip_code}"
+#   create_address(book, Faker::Name.name, address, Faker::Number.number(10))
+# end
+
+# --- DRIVER CODE ----- 
+to_exit = false
+
+puts "WOULD YOU LIKE TO ACCESS A CONTACT?"
+answer = gets.chomp
+if answer == "yes" || answer == "YES"
+  until to_exit
+    puts "ADD A CONTACT or FIND A CONTACT?"
+    action = gets.chomp
+    if action == "add a contact" || action == "ADD A CONTACT"
+      puts "NAME? "
+      name = gets.chomp
+      puts "ADDRESS?"
+      address = gets.chomp
+      puts "PHONE NUMBER?"
+      phone_number = gets.chomp
+      add_contact(db, name, address, phone_number)
+
+    elsif action == "find a contact" || action == "FIND A CONTACT"
+
+    else 
+      puts "Would you like to try again? To exit type 'exit'."
+      again = gets.chomp
+      if again == "exit"
+        to_exit = true
+      end
+    end
+  end
+elsif answer == "no" || answer == "NO"
+  puts "Have a nice day."
+else 
+  puts "Invalid answer. Please enter yes or no."
+end
+
+# error in loop, automatically asks to add contact  or find a contact immediately after adding contact
+
+
+
+
+
+
+
 
