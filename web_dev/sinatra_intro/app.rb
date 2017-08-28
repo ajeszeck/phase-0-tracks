8bb4fc834ce860905dd7d1426de26d2b8cf64242
Anna J <ajeszeck@gmail.com>
2017-08-27 18:13:12 -0700

    Add code of example for query parameter usage and of route parameter usage, attempt database communication - work in progress

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -8,28 +8,47 @@ db.results_as_hash = true
 # write a basic GET route
 # add a query parameter
 # GET /
+# get '/' do
+#   "#{params[:name]} is #{params[:age]} years old."
+# end
 get '/' do
-  "#{params[:name]} is #{params[:age]} years old."
+  adjective = params[:adjective]
+  if adjective
+    "Hello #{adjective} World!"
+  else
+    "Hello World!"
+  end
 end
 
 get '/contact' do
   "4287 89th Ave S, Portland, OR 82740"
 end
+
+get '/great_job' do
+  person = params[:person]
+  if person
+    "Good job, #{person}!"
+  else
+    "Good job!"
+  end
+end
+
 # write a GET route with
 # route parameters
-get '/about/:person' do
-  person = params[:person]
-  "#{person} is a programmer, and #{person} is learning Sinatra."
+
+get '/:number_1/plus/:number_2' do
+  sum = params[:number_1].to_i + params[:number_2].to_i
+  "The sum of #{params[:number_1]} plus #{params[:number_2]} is #{sum}."
 end
 
-get '/:person_1/loves/:person_2' do
-  "#{params[:person_1]} loves #{params[:person_2]}"
-end
+# get '/about/:person' do
+#   person = params[:person]
+#   "#{person} is a programmer, and #{person} is learning Sinatra."
+# end
 
-get '/great_job/:person_1' do
-  person = params[:person_1]
-  "Good job, #{person}!"
-end
+# get '/:person_1/loves/:person_2' do
+#   "#{params[:person_1]} loves #{params[:person_2]}"
+# end
 
 # write a GET route that retrieves
 # all student data
@@ -52,3 +71,26 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# THIS IS MY ATTEMPT, WILL PROBABLY RETURN TO FIX -- RAN OUT OF TIME
+# get '/students/:campus' do
+#   "#{params[:campus].to_s}"
+#   campus = db.execute("SELECT * FROM students WHERE campus=?", [params[:campus]])
+#   print campus
+#   response = ""
+#   campus.each do |student|
+#     response << "ID: #{student['id']}<br>"
+#     response << "Name: #{student['name']}<br>"
+#     response << "Age: #{student['age']}<br>"
+#     response << "Campus: #{student['campus']}<br><br>"
+#   end
+#   response
+# end
+
+
+## WEB APP LIBRARIES
+  # Ruby on Rails, Merb, Nitro, and Camping
+
+## WEB STACK = A Web stack is the collection of software required for Web development. 
+#  At a minimum, a Web stack contains an operating system (OS), a programming language, 
+#  database software and a Web server.
